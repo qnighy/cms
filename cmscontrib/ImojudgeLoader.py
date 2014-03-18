1dd7c9afed25c3d1516173e7c8b4446c264126ca
Masaki Hara <ackie.h.gmai@gmail.com>
2014-03-19 00:51:20 +0900

    Add credit to ImojudgeLoader.py.

--- cmscontrib/ImojudgeLoader.py
+++ cmscontrib/ImojudgeLoader.py
@@ -6,6 +6,7 @@
 # Copyright © 2010-2012 Stefano Maggiolo <s.maggiolo@gmail.com>
 # Copyright © 2010-2012 Matteo Boscariol <boscarim@hotmail.com>
 # Copyright © 2013-2014 Luca Wehrstedt <luca.wehrstedt@gmail.com>
+# Copyright © 2014 Masaki Hara <ackie.h.gmai@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
